4816b3127d22cdeb51fbca991aaa160f9a1cf430
Albert <albert.bayazidi@gmail.com>
2024-06-30 18:35:25 +0200

    nvim config

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+    "williamboman/mason.nvim",
+    'williamboman/mason-lspconfig.nvim',
+    
+    config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
+    end
+}
