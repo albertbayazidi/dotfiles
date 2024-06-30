f632e676fbbada68b50dfbd39adce6dea683bd7f
Albert <albert.bayazidi@gmail.com>
2024-06-30 20:06:54 +0200

    nvim config

--- nvim/lua/albert/remap.lua
+++ nvim/lua/albert/remap.lua
@@ -15,3 +15,27 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "gj")
 vim.keymap.set("n", "K", "gk")
+
+vim.api.nvim_create_autocmd('LspAttach',{
+    group = vim.api.nvim_create_augroup('UserLspConfig',
+        { clear = true }),
+    callback = function(e)
+    local opts = {buffer = e.buf}
+        vim.keymap.set("n", "gd",
+            function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "h",
+            function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "go",
+            function() vim.lsp.buf.type_definition() end, opts)
+        vim.keymap.set("n", "rn",
+            function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "[d",
+            function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "]d",
+            function() vim.diagnostic.goto_next() end, opts) 
+    end
+})
+
+
+
+
