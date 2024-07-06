30f935659d972e303eb585b4f4ffd4641e5906f7
Albert <albert.bayazidi@gmail.com>
2024-07-06 23:15:45 +0200

    nvim config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,10 +3,14 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>ff', builtin.find_files,
+            {desc = "fa"})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+            {desc = "fa"})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers,
+            {desc = "fa"})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+            {desc = "fa"})
 
     end
        }
