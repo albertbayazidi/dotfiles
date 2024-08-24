ccf987c910fcfaedb8d03ebdafe01813e3ae70b6
Albert <albert.bayazidi@gmail.com>
2024-08-24 12:19:07 +0200

    reformating and adding new past behavior

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,80 +1,85 @@
 return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "williamboman/mason.nvim",
     "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "williamboman/mason.nvim",
-        "neovim/nvim-lspconfig",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-    },
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+  },
 
-    config = function()
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    })
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "gopls",
+        "lua_ls",
+        "astro",
+        "tinymist",
+        "html",
+        "htmx",
+        "clangd",
+      },
 
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "gopls",
-                "lua_ls",
-                "astro",
-                "html",
-                "htmx",
-          },
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup {}
+        end
+      }
+    })
+    local cmp        = require("cmp")
+    local cmp_select
+                     = { behavior = cmp.SelectBehavior.Select }
 
-            handlers = {
-                function (server_name)
-                    require("lspconfig")[server_name].setup {}
-                end
-            }
-        })
-        local cmp = require("cmp")
-        local cmp_select
-                = {behavior = cmp.SelectBehavior.Select}
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(
+          cmp_select,
+          { desc = "Pick prev suggestion" }),
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(
-                    cmp_select,
-                    {desc = "Pick prev suggestion"}),
+        ["<C-n>"] = cmp.mapping.select_next_item(
+          cmp_select,
+          { desc = "Pick next suggestion" }),
 
-                ["<C-n>"] = cmp.mapping.select_next_item(
-                    cmp_select,
-                    {desc = "Pick next suggestion"} ),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }
+        , { desc = "confirm" }),
 
-                ["<C-y>"] = cmp.mapping.confirm({select = true}
-                    ,{desc = "confirm"}),
+        ["<C-Space>"] = cmp.mapping.complete(),
 
-                ["<C-Space>"] = cmp.mapping.complete(),
-
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp"},
-                { name = "luasnip"},
-                { name = "path"},
-            },
-                {{name = "buffer"},})
-        })
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({
-            settings = {Lua = {diagnostics =
-                {globals = { "vim","none" }}}}
-        })
-    end
+      }),
+      sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        },
+        { { name = "buffer" }, })
+    })
+    local lspconfig = require("lspconfig")
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics =
+          { globals = { "vim", "none" } }
+        }
+      }
+    })
+  end
 }
