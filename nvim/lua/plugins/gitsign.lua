c02379d442a193b9a5a82e35567fa40d1d332b51
Albert <albert.bayazidi@gmail.com>
2024-09-24 12:51:03 +0200

    added julia support

--- nvim/lua/plugins/gitsign.lua
+++ nvim/lua/plugins/gitsign.lua
@@ -1,15 +1,15 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        local gitsigns = require("gitsigns")
-        gitsigns.setup({
-            signs = {
-                add = { text = "+" },
-                change = { text = "~" },
-                delete = { text = "-" },
-                topdelete = { text = "‾" },
-                untracked = { text = "┆" },
-            },
-        })
-    end
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gitsigns = require("gitsigns")
+    gitsigns.setup({
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "-" },
+        topdelete = { text = "‾" },
+        untracked = { text = "┆" },
+      },
+    })
+  end
 }
