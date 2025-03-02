3d59a3b9d78d8bfbea287ec21da6d80a019f8e32
Albert <albert.bayazidi@gmail.com>
2025-03-02 12:38:51 +0100

    added support for hunk preview, and removal

--- nvim/lua/plugins/gitsign.lua
+++ nvim/lua/plugins/gitsign.lua
@@ -2,6 +2,8 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     local gitsigns = require("gitsigns")
+
+    -- Setup for gitsigns
     gitsigns.setup({
       signs = {
         add = { text = "+" },
@@ -11,5 +13,21 @@ return {
         untracked = { text = "┆" },
       },
     })
+
+    -- Helper
+    local function map(mode, lhs, rhs, opts)
+      opts = opts or {}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Keybindings
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+    map('n', '<leader>hs', gitsigns.stage_buffer)
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
   end
 }
