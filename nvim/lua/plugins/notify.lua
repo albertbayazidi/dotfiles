14e64df7425676808c9d104fed7e80296df6ad2c
Albert <albert.bayazidi@gmail.com>
2025-03-02 12:42:13 +0100

    added support for notfiy so one can more esaly see notifcations from lsp or nvim

---
+++ nvim/lua/plugins/notify.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      local notify = require("notify")
+      notify.setup({
+        stages = "fade_in_slide_out",  -- Smooth animations
+        timeout = 3000,                -- Notification timeout in ms
+        background_colour = "#000000", -- Background color
+      })
+      vim.notify = notify              -- Replace default `vim.notify`
+
+      -- Hook LSP messages into nvim-notify
+      vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        local lvl = ({ "ERROR", "WARN", "INFO", "DEBUG" })[result.type]
+        notify(result.message, lvl, { title = client and client.name or "LSP" })
+      end
+    end,
+  },
+}
