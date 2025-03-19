a538e6328a3a8cd15b214ff9afcb5c4a6f935cd3
Albert <albert.bayazidi@gmail.com>
2025-03-19 13:28:39 +0100

    added back some missing files

---
+++ bspwm/check_for_monitor.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+my_laptop_external_monitor=$(xrandr --query | grep -w 'DP-1')
+if [[ $my_laptop_external_monitor != *disconnected* ]]; then
+    xrandr --output DP-1 --primary --mode 1920x1080 --rotate normal --output eDP-1 --mode 1920x1080 --rotate normal --left-of DP-1
+    bspc monitor eDP-1 -d I II III  
+    bspc monitor DP-1 -d IV V VI VII 
+    polybar external & disown
+    polybar mybar & disown 
+
+  else
+    xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal
+    bspc monitor eDP-1 -d I II III IV V 
+    polybar laptop & disown
+fi
+
