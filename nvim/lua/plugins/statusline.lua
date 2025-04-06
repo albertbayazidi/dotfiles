833550cbca25c76b93efb42bd983a79011541450
Albert <albert.bayazidi@gmail.com>
2025-04-06 21:19:10 +0200

    updated lualine to be more modular

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -1,42 +1,77 @@
-return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-          statusline = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = true,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        }
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch',
-          { 'diff', colored = true,
-            symbols = { added = '',
-              modified = '~',
-              removed = '' } } },
+local separators = {
+    component = { left = '', right = '' },
+    section = { left = '', right = '' },
+}
 
-        lualine_c = { 'filename' },
-        lualine_x = { 'diagnostics', 'fileformat' },
-        lualine_y = { 'filetype' },
-        lualine_z = { 'progress' }
-      },
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {}
-    }
-  end
+local diff_component = {
+    'diff',
+    colored = true,
+    symbols = {
+        added = '',
+        modified = '~',
+        removed = '',
+    },
+}
+
+local options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = separators.component,
+    section_separators = separators.section,
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+        statusline = 200,
+        tabline = 500,
+        winbar = 300,
+    },
+}
+
+local filename = {
+    'filename',
+    file_status = true,
+    newfile_status = false,
+    path = 0,
+
+    shorting_target = 20,
+
+    symbols = {
+        modified = '[+]',
+        readonly = '[-]',
+        unnamed = '[No Name]',
+        newfile = '[New]',
+    },
+}
+
+local lsp_status = {
+    'lsp_status',
+    icon = '',
+    symbols = {
+        spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+        done = '',
+        separator = ' ',
+    },
+    ignore_lsp = {},
+}
+
+local sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', diff_component },
+    lualine_c = { filename },
+
+    lualine_x = { 'diagnostics', 'fileformat', lsp_status },
+    lualine_y = { 'searchcount', 'filetype' },
+    lualine_z = { 'progress' },
+}
+
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+            options = options,
+            sections = sections,
+            inactive_section = sections,
+        }
+    end,
 }
