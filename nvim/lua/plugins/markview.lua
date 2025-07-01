c0daa1bf5237eac8f733f49b782da5fd4757f49f
Albert <albert.bayazidi@gmail.com>
2025-07-01 19:08:53 +0200

    fixed order of loading to avoid error

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,16 +1,17 @@
 return {
-  "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-  event = "VeryLazy",
-  config = function()
-    require("markview").setup({
-      preview = {
-        filetypes = {
-          "markdown",
-          "mdx",
-        },
-        ignore_buftypes = { "typst" },
-      },
-    })
-  end
+    "OXY2DEV/markview.nvim",
+    lazy = false,  -- Recommended
+    event = "VeryLazy",
+    priority = 49, -- lower order then tree-sitter to avoid error
+    config = function()
+        require("markview").setup({
+            preview = {
+                filetypes = {
+                    "markdown",
+                    "mdx",
+                },
+                ignore_buftypes = { "typst" },
+            },
+        })
+    end
 }
