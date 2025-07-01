6246cf7c1bc10d793fa48c52f306a85324e3c29b
Albert <albert.bayazidi@gmail.com>
2025-07-01 19:08:53 +0200

    fixed order of loading to avoid error

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,25 @@
-return { {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+return {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { 'markview.nvim' },
+    branch = 'master',
+    build = ":TSUpdate",
+    lazy = false,
+    priority = 50,
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = {
-        "c", "cmake", "go",
-        "bash", "lua", "sql", "python", "julia",
-        "astro", "typescript", "javascript", "html",
-        "typst",
-        "json"
-      },
+        configs.setup({
+            ensure_installed = {
+                "c", "cmake", "go",
+                "bash", "lua", "sql", "python", "julia",
+                "astro", "typescript", "javascript", "html",
+                "typst", "markdown", "markdown_inline",
+                "json"
+            },
 
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
-} }
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
