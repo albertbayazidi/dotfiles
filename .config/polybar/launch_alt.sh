ed62a8e0e0330b633e6d603d196fc255492b2730
albert <albert.bayzidi@gmail.com>
2025-09-02 07:44:27 +0200

    updated readme, made alt super r shurtcut use new monitor script

--- .config/polybar/launch_alt.sh
+++ .config/polybar/launch_alt.sh
@@ -2,8 +2,8 @@
 
 CONFIG_DIR=$HOME/.config/polybar/config.ini
 
-polybar external -c $CONFIG_DIR & 
-polybar left-alt -c $CONFIG_DIR &
-polybar right -c $CONFIG_DIR &
+polybar external -c $CONFIG_DIR & disown
+polybar left-alt -c $CONFIG_DIR & disown 
+polybar right -c $CONFIG_DIR & disown 
 
 
