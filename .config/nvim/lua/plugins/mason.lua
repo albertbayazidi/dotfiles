6c1f1b6483277af12e905cb7c31cc53813591964
albert <albert.bayzidi@gmail.com>
2025-09-03 22:13:11 +0200

    removed some unneeded/duplicate code
    mason will automatically start with some config, this is good, and i use it all we use it all the time.
    however if you try adding formatting setting to nvim_lspconfig it will also start up a lsp, meaning you get two, one with and one without formating. This makes things act wired.
    never figured out how i can get formating for lua_ls

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,25 +1,21 @@
 return {
-  require("mason").setup({
-    ui = {
-      icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
-      }
-    }
-  }),
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-        "lua_ls",
-        "astro",
-        "tinymist",
-        "html",
-        "clangd",
-    },
-    handlers = {
-      function(server_name)
-        require("lspconfig")[server_name].setup {}
-      end
-    }
-  })
+    require("mason").setup({
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    }),
+    require("mason-lspconfig").setup({
+        ensure_installed = {
+            "lua_ls",
+            "astro",
+            "tinymist",
+            "html",
+            "clangd",
+        },
+    })
+
 }
