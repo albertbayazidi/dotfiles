2ad1c52c8da5e49aee350f11a12931b96809ba35
albert <albert.bayzidi@gmail.com>
2025-09-06 10:06:12 +0200

    added limit to volume, and a block list to telesc

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,14 +3,20 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        local telecsope_builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', telecsope_builtin.find_files,
-            { desc = "fa" })
-        vim.keymap.set('n', '<leader>fg', telecsope_builtin.live_grep,
-            { desc = "fa" })
-        vim.keymap.set('n', '<leader>fb', telecsope_builtin.buffers,
-            { desc = "fa" })
-        vim.keymap.set('n', '<leader>fh', telecsope_builtin.help_tags,
-            { desc = "fa" })
+        local telescope = require('telescope')
+        telescope.setup {
+            defaults = {
+                file_ignore_patterns = {
+                    "backups"
+                }
+            }
+        }
+
+        local telescope_builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = "fa" })
+        vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = "fa" })
+        vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = "fa" })
+        vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = "fa" })
     end
 }
