306d67cdfaf6251eb198874779ac5034b4e3560b
albert <albert.bayazidi@gmail.com>
2025-11-09 15:18:13 +0100

    moving to native lsp, only lua works now

---
+++ .config/nvim/lua/albert/lsp/lsp_keymaps.lua
@@ -0,0 +1,18 @@
+vim.api.nvim_create_autocmd("LspAttach",{
+  group = vim.api.nvim_create_augroup("lsp-attach", {clear = true}),
+  callback = function(evnet)
+		local function map(keys,func)
+			vim.keymap.set("n", keys,func,{ buffer = evnet.buf})
+    end
+
+    map("H", vim.lsp.buf.hover)
+    map("gs", vim.lsp.buf.signature_help)
+    map("gd", vim.lsp.buf.declaration)
+    map("]d", vim.diagnostic.goto_next)
+    map("[d", vim.diagnostic.goto_prev)
+    map("<leader>ca", vim.lsp.buf.code_action)
+    map("<leader>fq", vim.lsp.buf.format)
+
+  end
+
+})
