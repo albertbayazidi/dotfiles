fd80aa573cdad9600425954aa622c719c65ff879
albert <albert.bayzidi@gmail.com>
2025-11-19 08:07:43 +0100

    applaying formating

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,38 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = { 'markview.nvim' },
-    branch = 'master',
-    build = ":TSUpdate",
-    lazy = false,
-    priority = 50,
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { "markview.nvim" },
+  branch = "master",
+  build = ":TSUpdate",
+  lazy = false,
+  priority = 50,
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {
-                "c", "cmake", "go",
-                "bash", "lua", "sql", "python", "julia",
-                "astro", "typescript", "javascript", "html",
-                "typst", "markdown", "markdown_inline",
-                "json"
-            },
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "cmake",
+        "go",
+        "bash",
+        "lua",
+        "sql",
+        "python",
+        "julia",
+        "astro",
+        "typescript",
+        "javascript",
+        "html",
+        "typst",
+        "markdown",
+        "markdown_inline",
+        "json",
+        "java",
+        "svelte",
+      },
 
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
