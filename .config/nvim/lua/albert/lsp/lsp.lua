859499741f170943384e4dfc28da32597133bc76
albert <albert.bayazidi@gmail.com>
2025-11-20 07:17:15 +0100

    fixed go to issue, and added formating to typst
    Still having problems with autocomplete. lsp only wanting to autocomplete word
    related to programing languages and not also words already in the document
    in typst we have the added issue that it only want to autocomplete when \# is first written

--- .config/nvim/lua/albert/lsp/lsp.lua
+++ .config/nvim/lua/albert/lsp/lsp.lua
@@ -13,6 +13,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method("textDocument/completion") then
+      local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      client.server_capabilities.completionProvider.triggerCharacters = chars
       vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
       vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
       vim.keymap.set("i", "<C-Space>", function() vim.lsp.completion.get() end)
