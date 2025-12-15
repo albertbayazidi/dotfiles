a5586bdab518c0106d2c7c6045ab0c62482d0e05
albert <albert.bayazidi@gmail.com>
2025-12-15 17:54:10 +0100

    made monitor mode picker in rofi

---
+++ .config/rofi/applets/bin/monitor_mode.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+source "$HOME/.config/rofi/applets/shared/theme.bash"
+theme="$type/$style"
+
+# Theme Elements
+prompt="Monitor mode"
+mesg="Pick monitor mode"
+
+list_col="3"
+list_row="1"
+
+# Options
+option_1="󱎴"
+option_2="󰍺"
+option_3="󱢇"
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+		-theme-str 'textbox-prompt-colon {str: "󰍹";}' \
+		-dmenu \
+		-p "$prompt" \
+		-mesg "$mesg" \
+		-theme ${theme}
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$option_1\n$option_2\n$option_3" | rofi_cmd
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $option_1) $HOME/.config/bspwm/extend_monitor.sh ;;
+    $option_2) $HOME/.config/bspwm/duplicate_monitor.sh ;; 
+    $option_3) notify-send -u critical "Obs!" "New modes under construction" ;; 
+esac
+
