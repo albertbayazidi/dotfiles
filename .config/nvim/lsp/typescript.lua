74e1da7317747f41d82f1bdde2bcf7986a301bd0
albert <albert.bayazidi@gmail.com>
2026-01-20 19:11:44 +0100

    forgot to add typescript f, and chagnged gitignore

---
+++ .config/nvim/lsp/typescript.lua
@@ -0,0 +1,10 @@
+vim.lsp.config["typescript"] = {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
+  root_markers = { ".git" },
+}
