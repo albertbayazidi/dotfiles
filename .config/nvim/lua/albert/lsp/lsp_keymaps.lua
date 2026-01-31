a38d83eb4d5b27ae012ba781c1e5efc81d646ac8
albert <albert.bayazidi@gmail.com>
2026-01-31 19:52:16 +0100

    added go, bash lsps

--- .config/nvim/lua/albert/lsp/lsp_keymaps.lua
+++ .config/nvim/lua/albert/lsp/lsp_keymaps.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 -- Starting and stopping harper
 vim.api.nvim_create_user_command("StopHarper", function()
-  vim.lsp.enable("harper",false) -- Want this to be using stop or disable not enable with false flag
+  vim.lsp.enable("harper", false) -- Want this to be using stop or disable not enable with false flag
 end, { desc = "stops harper from giving hints" })
 
 vim.api.nvim_create_user_command("StartHarper", function()
